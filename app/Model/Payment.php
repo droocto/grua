69f08614fcfc2d0dbdc59a4f68d073809df15310
DrOcto <alejo.debian@gmail.com>
2012-10-01 11:17:11 -0500

    Este modelo no tiene conexión con otro porque es una tabla para paremetrizacion #2

---
+++ app/Model/Payment.php
@@ -0,0 +1,14 @@
+<?php
+#doc
+#    classname:    ClassName
+#    scope:        PUBLIC
+#
+#/doc
+
+class Payment extends AppModel
+{
+    public $useName = 'Payment';
+    public $useTable = 'payments';
+
+}
+?>
